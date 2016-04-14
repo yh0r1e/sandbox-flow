0a636278ff4b8978fea8a032e68425ec7e688228
yhorie <yuk.horie@gmail.com>
2016-04-15 05:49:03 +0900

    try examples

---
+++ src/call-main-function.js
@@ -0,0 +1,5 @@
+// @flow
+
+var length1 = require('./main.js');
+
+var total1 = length1('Hello') + length1(null);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,49 @@
 // @flow
 
+// BAD
 function foo(x) {
   return x * 10;
 }
+
 foo('Hello, world!');
+
+
+
+// BAD
+function length1(x) {
+  return x.length;
+}
+
+module.exports = length1;
+
+//var total1 = length1('Hello') + length1(null);
+
+// GOOD
+function length2(x) {
+  if (x !== null) {
+    return x.length;
+  } else {
+    return 0;
+  }
+}
+
+var total2 = length2('Hello') + length2(null);
+
+
+// BAD
+function foo1(x) {
+  return x.length;
+}
+
+var res1 = foo1('Hello') + foo1(42);
+
+// GOOD
+function foo2(x) {
+  if (typeof x === 'string') {
+    return x.length;
+  } else {
+    return x;
+  }
+}
+
+var res2 = foo2('Hello') + foo2(42);
